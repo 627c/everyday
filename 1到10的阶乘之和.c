a1a2d5444c453c9a68cf12b509b18229ff08cfb5
627c <liuanqi422609@163.com>
2023-10-29 15:24:10 +0800

    提交代码

--- 1到10的阶乘之和.c
+++ 1到10的阶乘之和.c
@@ -3,10 +3,11 @@ int main()
 {
 	int i=0;
 	int p;
-	long int q=1;
+	int q=1;
 	long int sum=0;
 	for(i=1;i<=10;i++)
 	{
+		q=1;
 		for(p=1;p<=i;p++)
 		{
 			q=q*p;
@@ -15,3 +16,18 @@ int main()
 	}
 	printf("%d\n",sum);
 }
+//{
+//	int i=0;
+//	int p;
+//	long int q=1;
+//	long int sum=0;
+//	for(i=1;i<=10;i++)
+//	{
+//		for(p=1;p<=i;p++)
+//		{
+//			q=q*p;      错误原因，q的值不会赋初值1，会一直保留，不会置1，应该在循环中重新设置，不能用static,具体看static用法
+//		}
+//		sum+=q;
+//	}
+//	printf("%d\n",sum);
+//}
