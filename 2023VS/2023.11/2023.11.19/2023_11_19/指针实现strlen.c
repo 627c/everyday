f8568f87ec00355a31f51198077758a52a52e705
627c <liuanqi422609@163.com>
2023-11-19 22:28:17 +0800

    提交代码

---
+++ 2023VS/2023.11/2023.11.19/2023_11_19/指针实现strlen.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	char* start = str;
+	char* end = str;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	return end - start;
+}
+int main()
+{
+	//strlen-求字符串长度
+	//递归-模拟实现了strlen-计数器的方式1，递归的方式2
+	char arr[] = "jiangnan";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+}
