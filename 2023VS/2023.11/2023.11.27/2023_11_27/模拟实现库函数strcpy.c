30f807dbaf2a4c0acca79e689ef6773d51f464ec
627c <liuanqi422609@163.com>
2023-11-27 16:41:47 +0800

    提交代码

---
+++ 2023VS/2023.11/2023.11.27/2023_11_27/模拟实现库函数strcpy.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void my_strcpy(char* dest, char* src)
+{
+	while(*src!='\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = *src;     //\0µÄ¿½±´
+}
+int main()
+{
+	//strcpy  ×Ö·û´®¿½±´
+	char arr1[] = "###############";
+	char arr2[] = "jiangnan";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
