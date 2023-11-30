b37e6ad317b79e12d53778f609c6bbcc9895fc82
627c <liuanqi422609@163.com>
2023-11-30 16:21:14 +0800

    提交代码

---
+++ 2023VS/2023.11/2023.11.30/2023_11_30/求整数数组第i1到i2元素的和.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int sum(int x[], int n)
+{
+	int i, s = 0;
+	for (i = 0; i < n; i++)
+		s = s + x[i];
+	return s;
+}
+int main()
+{
+	int i, i1, i2, result, x[10];
+	for (i = 0; i < 10; i++) scanf("%d", &x[i]);
+	scanf("%d%d", &i1, &i2);
+	result = sum(x + i1,i2 - i1 + 1);
+	printf("sum=%d\n", result);
+	return 0;
+}
