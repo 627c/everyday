bf46563731879f6ca84422604adf35726d1fc83d
627c <liuanqi422609@163.com>
2024-01-12 21:25:33 +0800

    提交代码

---
+++ 2024/2024.1/2024.1.12/2024_1_12/Contact_Dynamic.h
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+//#define MAX 1000
+#define MAX_NAME 20
+#define MAX_TELE 12
+#define MAX_SEX 5
+#define MAX_ADDR 30
+#define DEFAULT_SZ 3
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	char tele[MAX_TELE];
+	char sex[MAX_SEX];
+	char addr[MAX_ADDR];
+	int age;
+};
+struct Contact
+{
+	struct PeoInfo* data;//指向一块空间，通讯录大小
+	int size;//记录当前已经有的元素
+	int capacity;//记录当前通讯录最大容量
+};
+//声明函数
+//初始化通讯录的函数
+void InitContact(struct Contact* ps);
+//增加一个信息到通讯录
+void AddContact(struct Contact* ps);
+//打印通讯录中的信息
+void ShowContact(const struct Contact* ps);
+//删除指定联系人
+void DelContact(struct Contact* ps);
+//查找指定人的信息
+void SearchContact(const struct Contact* ps);
+//修改指定联系人
+void ModifyContact(struct Contact* ps);
+//排序通讯录内容
+void SortContact(struct Contact* ps);
+//清除动态开辟的空间
+void DestoryContact(struct Contact* ps);
+//保存文件
+void SaveContact(struct Contact* ps);
+//加载文件中的信息到通讯录
+void LoadContact(struct Contact* ps);
