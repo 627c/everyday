4ff914eacaf9af6c5c04f4eed0125d7599f510f8
627c <liuanqi422609@163.com>
2024-03-05 21:06:57 +0800

    提交代码matlab

---
+++ matlab/m2024_3_5_1.m
@@ -0,0 +1,30 @@
+%二分法求方程实根
+%自动化2301 刘安琪 1104220109
+f=@(x)x^6-x-1;
+a=1;
+b=2;
+k=1;
+while (b-a)>=0.0005 %精确到小数后三位
+    x=(a+b)/2;
+    if f(x)==0
+        fprintf('%d\n',k);
+        fprintf('%.3f',x);
+    else
+        if f(a)*f(x)<0
+            b=x;
+        else
+            a=x;
+        end
+    end
+    k=k+1;
+    if k>50
+        break;
+    end
+end
+x=(a+b)/2;
+fprintf('%d\n',k);
+fprintf('%.3f',x);
+
+
+
+
